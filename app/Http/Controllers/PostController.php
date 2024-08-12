be7c45674566c2b489e5249a8dd8cbc5b2470354
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2024-08-13 00:47:22 +0300

    Сategory output

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
@@ -68,27 +69,16 @@ class PostController extends Controller
         return view('post.view', compact('post', 'prev', 'next'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Post $post)
+    public function byCategory(Category $category)
     {
-        //
-    }
+        $posts = Post::query()
+            ->join('category_post', 'posts.id', '=', 'category_post.post_id')
+            ->where('category_post.category_id', '=', $category->id)
+            ->where('active', '=', true)
+            ->whereDate('published_at', '<=', Carbon::now())
+            ->orderBy('published_at', 'desc')
+            ->paginate(10);
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Post $post)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Post $post)
-    {
-        //
+        return view('home', compact('posts', 'category'));
     }
 }
--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -2,25 +2,27 @@
 
 namespace App\View\Components;
 
+use App\Models\Category;
 use Closure;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 class AppLayout extends Component
 {
     /**
-     * Create a new component instance.
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
-     * Get the view / contents that represent the component.
+     * Get the view / contents that represents the component.
      */
     public function render(): View|Closure|string
     {
-        return view('layout.app');
+        $categories = Category::query()
+            ->join('category_post', 'categories.id', '=', 'category_post.category_id')
+            ->select('categories.title', 'categories.slug', DB::raw('count(*) as total'))
+            ->groupBy(['categories.title', 'categories.slug'])
+            ->orderByDesc('total')
+            ->limit(5)
+            ->get();
+
+        return view('layout.app', compact('categories'));
     }
 }
