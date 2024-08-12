c78cdc99fb1d684ea486fad8a6d79bdcd9519568
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2024-08-12 22:46:49 +0300

    Post page

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('/{post:slug}', [PostController::class, 'show'])->name('view');
